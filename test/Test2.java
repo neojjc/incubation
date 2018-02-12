87a144b48f96201afcadf30068bb90ef678bf54d
Junchul, Jung <junchul.jung@linecorp.com>
2018-02-12 20:25:41 +0900

    Test2.java, Test3.java committed May the force be with you

--- test/Test2.java
+++ test/Test2.java
@@ -1,6 +1,8 @@
 public class Test2
 {
     public static void main( String[] args ){
-        System.out.println( "Hello World, Hello World" );
+        System.out.println( "Hello World, Hello World1" );
+        System.out.println( "Hello World, Hello World2" );
+        System.out.println( "Hello World, Hello World3" );
     }
 }
