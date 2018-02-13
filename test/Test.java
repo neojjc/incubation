99f65dddfa7a4b70de9a5f1d0fdad83a1d58e9dc
Junchul, Jung <junchul.jung@linecorp.com>
2018-02-13 11:21:31 +0900

    AOA

--- test/Test.java
+++ test/Test.java
@@ -2,5 +2,7 @@ public class Test
 {
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
+		String a = "Void main Void";
+		System.out.println( a );
     }
 }
