6a7db7f0ddf1032f5408c5dc82c6e9d6110e2d1c
Junchul, Jung <junchul.jung@linecorp.com>
2018-02-13 11:10:09 +0900

    modified

--- remote/RemoteTest3.java
+++ remote/RemoteTest3.java
@@ -3,6 +3,10 @@ public class Remotetest3
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        String a = "TEST";
+        String b = "TEST";
+        String c = "TEST";
+        
 
     }
 }
